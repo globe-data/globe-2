e598cf2247a064a73d07a3d63df21331b7255635
Archie Wyles <archiewyles@gmail.com>
2025-01-02 18:25:17 +0000

    remove api

--- app/api/router.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from app.api.routes.analytics import analytics_router
-from app.api.routes.sessions import sessions_router
-
-# Create and export a single router instance
-api_router = APIRouter()
-
-api_router.include_router(analytics_router, prefix="/analytics")
-api_router.include_router(sessions_router, prefix="/sessions")
--- app/db/__init__.py
+++
@@ -1,3 +0,0 @@
-from .mongodb import db
-
-__all__ = ["db"]
